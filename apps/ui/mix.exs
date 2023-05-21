0b1f3bf651c5d8de9495f579b1b0f465aba95c68
Alexandre Sagette <alexandre.sagette@chanel.com>
2023-05-21 16:13:36 +0200

    clean

--- apps/ui/mix.exs
+++ apps/ui/mix.exs
@@ -25,7 +25,7 @@ defmodule Ui.MixProject do
   def application do
     [
       mod: {Ui.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :os_mon]
     ]
   end
 
